25dc664b3d06428503310263eee4d992651ae1f0
Erik <Erik@PC>
2019-06-09 11:38:53 +0200

    base map layout

--- sokoban/src/sokoban/graphics.java
+++ sokoban/src/sokoban/graphics.java
@@ -2,6 +2,7 @@ package sokoban;
 
 import java.awt.BorderLayout;
 import java.awt.Canvas;
+import java.awt.Color;
 import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
@@ -21,64 +22,38 @@ import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
+import chessboard.Main;
+
 public class graphics extends JPanel {
 
-	
 	static JTextField textfield1;
 	private BufferedImage image;
 
 	public graphics() {
-	       try {                
-	          image = ImageIO.read(new File("../../images/test.png"));
-	       } catch (IOException ex) {
-	            // handle exception...
-	       }
-	    }
-	
-	
-	public void paint() {
-		
-		
+		try {
+			image = ImageIO.read(new File("/images/red.png"));
+		} catch (IOException ex) {
+			// handle exception...
+		}
+
 		JFrame frame = new JFrame();
-		
-		
-		
-		
-		
-		
-		
-		
-		frame.add(createPanel());
-		frame.setPreferredSize(new Dimension(300,300));
+		frame.setSize(1920, 1080);
+		frame.getContentPane().add(new Main());
+		frame.setLocationRelativeTo(null);
+		frame.setBackground(Color.LIGHT_GRAY);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.pack();
 		frame.setVisible(true);
-		
-		
-		
-		
 	}
-	
-	
-	private JPanel createPanel() {
-        JPanel panel = new JPanel(new GridLayout(5,5));
-        ImageIcon icon = new ImageIcon("images/test.png");
-        JLabel one = new JLabel(icon);
-        JLabel two = new JLabel(icon);
-       
-        panel.add(one);
-        validate();
-        repaint();
-        panel.add(one);
-        
-        return panel;
-    }
 
-	    @Override
-	    protected void paintComponent(Graphics g) {
-	        super.paintComponent(g);
-	        g.drawImage(image, 0, 0, this); // see javadoc for more info on the parameters            
-	    }
+	public void paint(Graphics g) {
 
-	
+		for (int i = 20; i < 975; i += 75) {
+			for (int j = 50; j < 1800; j += 100) {
+				g.drawImage(image, j, i, this);
+
+			}
+
+		}
+	}
+
 }
