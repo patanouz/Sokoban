b5c0ca3944e41cd53ebbc33fc413e97b4971deaf
Erik <Erik@PC>
2019-07-28 17:48:18 +0200

    added TODO to main

--- sokoban/src/sokoban/Main.java
+++ sokoban/src/sokoban/Main.java
@@ -14,3 +14,30 @@ public class Main {
 		
 	}
 }
+
+/*
+
+Sokoban TODO:
+
+1. level editor!
+
+2. class for saving games as text, also loading maps from files.
+
+3. a win condition, example "next level" button.
+
+4. graphics updates
+
+5. a main menu
+
+6. a background image
+
+7. sick dubstep music
+
+8. easter eggs
+
+9. ???
+
+10. profit!
+
+
+*/
